36bda505962b11642b4a541c98ec51b5d4ef25b3
Paulo <nofnobre@gmail.com>
2026-05-15 21:43:27 +0100

    v_Observ_Forecast_Armaz_HIst_Observ

--- app/frontend/js/map-common.js
+++ app/frontend/js/map-common.js
@@ -1,10 +1,4 @@
 function criarMapa(idMapa) {
-
-    const portugalBounds = L.latLngBounds(
-        [36.8, -9.6],
-        [42.3, -6.0]
-    );
-
     const satellite = L.tileLayer(
         'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
         {
@@ -22,19 +16,39 @@ function criarMapa(idMapa) {
 
     const map = L.map(idMapa, {
         center: [39.5, -8.0],
-
         zoom: 7,
-
-        minZoom: 6,
-
-        maxBounds: portugalBounds,
-
-        maxBoundsViscosity: 1.0,
-
         layers: [satellite, labels]
     });
 
-    map.fitBounds(portugalBounds);
-
     return map;
+}
+
+async function carregarDistritoLeiria(map) {
+    const response = await fetch(GEOJSON_PATH);
+    const leiriaGeoJson = await response.json();
+
+    const leiriaLayer = L.geoJSON(leiriaGeoJson, {
+        style: {
+            color: 'blue',
+            weight: 3,
+            fillOpacity: 0.05
+        }
+    }).addTo(map);
+
+    const bounds = leiriaLayer.getBounds();
+    map.fitBounds(bounds);
+
+    return bounds;
+}
+
+function guardarLocalizacao(lat, lon) {
+    localStorage.setItem('selectedLat', lat);
+    localStorage.setItem('selectedLon', lon);
+}
+
+function obterLocalizacaoGuardada() {
+    return {
+        lat: localStorage.getItem('selectedLat'),
+        lon: localStorage.getItem('selectedLon')
+    };
 }
